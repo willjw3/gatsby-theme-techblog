24680831a4c15551e3cbce40dafce6b82c804294
willjw3 <will.j.ward3@gmail.com>
2019-08-21 11:41:35 +0900

    Removed node_modules

--- node_modules/theme-ui/src/context.js
+++
@@ -1,9 +0,0 @@
-import { createContext, useContext } from 'react'
-import { components } from './components'
-
-export const Context = createContext({
-  theme: {},
-  components,
-})
-
-export const useThemeUI = () => useContext(Context)
